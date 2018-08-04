e66248571d36bde65589fea622a0e4eed8b377cf
Igor Soroka <stupbird@gmail.com>
2018-08-05 00:41:34 +0300

    adding sationId and filtering asterisk out of prices, getting rid of crap from development stage

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').load();
+// require('dotenv').load();
 let ApiBuilder = require('claudia-api-builder'),
     api = new ApiBuilder(),
     cheerio = require('cheerio'),
@@ -6,7 +6,7 @@ let ApiBuilder = require('claudia-api-builder'),
     iconv = require('iconv-lite');
 
 const url = 'https://www.polttoaine.net/';
-const token = process.env.MAP_TOKEN;
+// const token = process.env.MAP_TOKEN;
 
 module.exports = api;
 
@@ -75,88 +75,33 @@ api.get('/city/{name}', function (req) {
 function pushGasStationsForLocation(body) {
     $ = cheerio.load(body);
     let prices = [];
-    let coords = getCoordinates(body);
     const priceTable = $('#Hinnat').find('.e10');
     const rows = priceTable.find('> tbody > tr');
     const regExpString = /[\w-]*E10[\w-]*/g;
     const yearNow = new Date().getFullYear();
-    //link for the map site
-    // let additionalUrl = rows.find('> td > a').attr('href');
     rows.each(function() {
         if ($(this).attr('class').match(regExpString)) {
             let values = [];
             $(this).find('td').each (function() {
                 values.push($(this).text())
             });
-            let address = values[0].split(", ")[1];
             let coordSite = $(this).find('> td > a').attr('href');
+            let stationId = typeof coordSite == "undefined" ? "-" : coordSite.split("id=")[1];
             let jsonObj = {
+                "id" : stationId,
                 "station" : values[0].replace(/\(.*\)/g, '').replace(/\u00B7/g, '').trim(),
                 "lastModified" : values[1] + yearNow,
-                "ninetyFive" : values[2],
-                "ninetyEight" : values[3],
-                "diesel" : values[4]
+                "ninetyFive" : values[2].replace("*", ""),
+                "ninetyEight" : values[3].replace("*", ""),
+                "diesel" : values[4],
+                "lat" : "-",
+                "lon" : "-"
             };
             prices.push(jsonObj);
         }
     });
     return {stations : prices};
 }
-
-function getAddresses(prices) {
-    let addresses = [];
-    for (var key in prices.stations) {
-        if (prices.stations.hasOwnProperty(key)) {
-            addresses.push(filterStationAddress(prices.stations[key].station));
-        }
-    }
-    return addresses;
-}
-
-function filterStationAddress(string) {
-    return string.split(', ')[1]
-        .replace(/\(.*\)/g, '')
-        .replace(/\[.*\]/g, '');
-}
-
-function getBodyWithCoordinates(urlEnding) {
-    if (urlEnding === "-") {
-      return [];
-    }
-    return new Promise(function(resolve, reject){
-      request({
-        method: "GET",
-        uri: url + urlEnding,
-        json: true    
-      }, function (err, response, body) {
-          if (err) return reject(err);
-          resolve(getCoordinatesFromScript(body));
-      });
-    });
-  }
-  
-  function getCoordinatesFromScript(body) {
-    $ = cheerio.load(body);
-    let coordsArray = [];
-    $('.centerCol').each(function() {
-      let obj = $(this).find("script").attr("type","text/javascript");
-      let scriptText = obj[3].children[0].data;
-      coordsArray = scriptText.split(/google.maps.LatLng/)[1]
-                              .match(/\(([^)]+)\)/)[1]
-                              .split(", ");
-    });
-    console.log(coordsArray);
-    return coordsArray;
-  }
-
-  function setCoordsForPrices(prices, coordsArray) {
-    for (var key in prices.stations) {
-        if (prices.stations.hasOwnProperty(key)) {
-            prices.stations[key].lat = coordsArray[0];
-            prices.stations[key].lon = coordsArray[1];
-        }
-    }
-  }
 
 api.get('/city/{name}/cheapestgas', function(req) {
     'use strict';
